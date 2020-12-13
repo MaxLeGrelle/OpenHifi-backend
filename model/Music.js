9cfe845de4d1680359cc9dd8a498673f4d289311
le Grelle Max <max.legrelle@student.vinci.be>
2020-12-13 12:20:10 +0100

    add - comment Music

--- model/Music.js
+++ model/Music.js
@@ -20,7 +20,7 @@ class Music {
     }
 
     /**
-    * Ajoute this à la liste des musiques et sauvegarde la liste modifiée
+    * Add this to the list of musics and save the modified list
     */
     save() {
         const musicList = getMusicsFromFile(FILE_PATH);
@@ -29,6 +29,11 @@ class Music {
         return true;
     }
 
+    /**
+     * create a new file which contains the muisc in base64
+     * @param {*} music64 music in base 64
+     * @param {*} titleMusic64 title of the music
+     */
     static saveMusic64(music64, titleMusic64) {
         let title = titleMusic64.replace("/","-") //replace '/' because writeFileSync will throw "no such file or directory"
         const timestamp = Date.now();
@@ -37,6 +42,10 @@ class Music {
         return path;
     }
 
+    /**
+     * Retrieve the base 64 music
+     * @param {*} pathMusic64 path to the file
+     */
     static getMusic64(pathMusic64) {
         if (!fs.existsSync(pathMusic64)) return null;
         const music64 = fs.readFileSync(pathMusic64);
@@ -45,11 +54,11 @@ class Music {
     }
 
     /**
-     * Met à jour de maniére asynchrone le nombre de like d'une musique. Return false en cas d'erreur et true si le nombre de like a changé.
-     * Si la musique correspondant à l'id de musicId a déjà été liké par l'utilisateur correspondant à l'id de userId alors la musique est dislike
-     * sinon la musique est like
-     * @param {*} musicId l'id de la musique à liker/disliker
-     * @param {*} userId l'id the l'utilisateur ayant like ou dislike
+     * Update asynchronously the number of likes of a music's id. 
+     * Return false if there's an error and true if the number of likes has been changed.
+     * If the music's id has already been liked by the user, then the music will be disliked else, the music is liked
+     * @param {*} musicId the music's id
+     * @param {*} userId the user's id
      */
     static async updateLikes(musicId, userId) {
         try {
@@ -73,9 +82,9 @@ class Music {
     }
 
     /**
-     * Retourne la musique correspond à l'id musicId. 
-     * Retourne l'erreur rencontré si il y en a eu une.
-     * @param {*} musicId l'id de la musique
+     * Return the music related to the music id musicId
+     * Return the error met of there's one
+     * @param {*} musicId music's id
      */
     static getMusicFromId(musicId) {
         const musicsList = Music.getList();
@@ -83,15 +92,15 @@ class Music {
     }
 
     /**
-     * recupere la liste des musiques
+     * Retrieve the list of musics from the json
      */
     static getList() {
         return getMusicsFromFile(FILE_PATH);
     }
 
     /**
-     * Retourne la liste des id des musiques créée par l'utilisateur dont l'id est idCreator.
-     * @param {*} idCreator l'id du créateur de la musique.
+     * Return the list of music's id created by the user with the id idCreator
+     * @param {*} idCreator creator's id
      */
     static getListMusicFromIdCreator(idCreator) {
         const musicList = Music.getList();
@@ -101,7 +110,7 @@ class Music {
     }
 
     /**
-     * Permet d'incrémenter de maniére automatique l'id d'une nouvelle musique.
+     * Automatically increment the id when a music is created
      */
     static incId() {
         const musicList = Music.getList();
@@ -111,9 +120,9 @@ class Music {
 }
 
 /**
- * écrit dans le fichier filePath pour sauvegarer la liste musicList
- * @param {*} path le chemin vers le fichier
- * @param {*} musicList la liste de musiques
+ * Write in the file path to save the list musicList
+ * @param {*} path the path to the file
+ * @param {*} musicList the list of musics
  */
 function saveMusicListToFile(path, musicList) {
     const musicListToJson = JSON.stringify(musicList);
@@ -121,9 +130,9 @@ function saveMusicListToFile(path, musicList) {
 }
 
 /**
- * Récupere la liste des musiques se trouvant dans filePath, 
- * return une liste vide si le fichier n'a pas été trouvé
- * @param {*} path le chemin vers le fichier
+ * Return the list of musics in path 
+ * return an empty list if path does not exist
+ * @param {*} path the path of the file
  */
 function getMusicsFromFile(path) {
     if (!fs.existsSync(path)) return [];
